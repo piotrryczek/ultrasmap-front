12a4579bf3176cc1e588814e681267ec1d43ef3c
Piotr Ryczek <htmlsalad@gmail.com>
2019-08-29 21:45:48 +0200

    Enviroments config and fix with redux devtools

--- src/config/store.js
+++ src/config/store.js
@@ -8,7 +8,7 @@ const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : compose,
   ),
 );
 
